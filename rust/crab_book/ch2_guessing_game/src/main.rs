5a63c663362c7874a958c240b11c3c232af55a0d
michael guldan <michaelguldan@gmail.com>
2022-06-04 17:36:48 -0700

    Rust "Crab Book" Ch. 2 excercise

---
+++ rust/crab_book/ch2_guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+// https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+
+        // create a string variable for the guess.
+        let mut guess = String::new();
+        // read in the string and fail gracefully if we can't read the string.
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            },
+        }
+    }
+}
